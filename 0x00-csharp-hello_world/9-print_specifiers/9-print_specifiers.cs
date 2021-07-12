5d9f5acc77c33bc6bbf74705ce9b9cf1c9af4c41
salahbesbes <salah_besbes@ymail.com>
2021-07-12 22:10:00 +0100

    Substring

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double percent = .7553;
+        double currency = 98765.4321;
+        string[] cultureNames = { "en-US" };
+        CultureInfo culture = new CultureInfo("en-US");
+        System.Console.WriteLine($"Percent: {percent:p1} tip");
+        System.Console.WriteLine(currency.ToString("C2", culture));
+    }
+}
