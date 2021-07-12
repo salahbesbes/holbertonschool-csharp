525559b738970ce572e8791caf9d2dce41927b70
salahbesbes <salah_besbes@ymail.com>
2021-07-12 17:58:03 +0100

    substring

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "Holberton School";
+        System.Console.WriteLine($"{str}{str}{str}");
+        System.Console.WriteLine(str.Substring(0, 9));
+    }
+}
