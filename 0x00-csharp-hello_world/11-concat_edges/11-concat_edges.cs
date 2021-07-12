3a268f9acd7e6d2411e82063fc335611c461862e
salahbesbes <salah_besbes@ymail.com>
2021-07-12 22:52:20 +0100

    avoid counting

--- 0x00-csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ 0x00-csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
-        str = $"{str.Substring(str.IndexOf("object-oriented"), "object-oriented".Length)} {str.Substring(str.IndexOf("programming"), "programming".Length)} {str.Substring(str.IndexOf("C#"), "C#".Length)}";
+        str = $"{str.Substring(49, 15)} {str.Substring(80, 11)} {str.Substring(119, 2)} {str.Substring(0, 2)}";
         System.Console.WriteLine(str);
     }
 }
