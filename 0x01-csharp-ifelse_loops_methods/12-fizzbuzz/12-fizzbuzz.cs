c7c6941a3facec1a823b6ca139e145f199e0c954
salahbesbes <salah_besbes@ymail.com>
2021-07-13 14:21:16 +0100

    fizz buzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sep = " ";
+            for (int nb = 0; nb <= 100; nb++)
+            {
+                if (nb == 100) sep = "\n";
+                if (nb % 15 == 0) { Console.Write($"FizzBuzz{sep}"); }
+                else if (nb % 3 == 0) { Console.Write($"Fizz{sep}"); }
+                else if (nb % 5 == 0) { Console.Write($"Buzz{sep}"); }
+                else Console.Write($"{nb}{sep}");
+            }
+        }
+    }
+}
