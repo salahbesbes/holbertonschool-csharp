dd27dcf8c6c999c0efe002f6031eb81050a54db4
salahbesbes <salah_besbes@ymail.com>
2021-07-13 12:16:16 +0100

    combo 1

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sep = ", ";
+            for (int i = 0; i <= 99; i++)
+            {
+                if (i == 99) sep = "\n";
+                System.Console.Write($"{i:D2}{sep}");
+            }
+        }
+    }
+}
