ded577cf5bc925a2656f146586f7c543ef71f8f8
salahbesbes <salah_besbes@ymail.com>
2021-07-13 13:30:31 +0100

    repeat char n times

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length <= 0) System.Console.WriteLine();
+        else
+        {
+            Console.WriteLine(new String('_', length));
+        }
+    }
+}
+
