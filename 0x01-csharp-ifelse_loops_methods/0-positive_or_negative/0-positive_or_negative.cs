98c49eb5d3d04aa7902739439ff2825bf035b2dc
salahbesbes <salah_besbes@ymail.com>
2021-07-13 11:20:36 +0100

    check positive

---
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        if (number > 0)
+        {
+            System.Console.WriteLine($"{number} is positive\n");
+        }
+        else if (number < 0)
+        {
+            System.Console.WriteLine($"{number} is negative\n");
+        }
+        else
+        {
+            System.Console.WriteLine($"{number} is zero\n");
+        }
+    }
+}
