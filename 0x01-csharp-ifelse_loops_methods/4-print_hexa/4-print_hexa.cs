58a40858f7f348b200302c4a12f1c7a418a8e0c6
salahbesbes <salah_besbes@ymail.com>
2021-07-13 11:56:15 +0100

    print hexa

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 98; i++)
+            {
+                System.Console.Write($"{i:D} = 0x{i:X}\n");
+            }
+        }
+    }
+}
