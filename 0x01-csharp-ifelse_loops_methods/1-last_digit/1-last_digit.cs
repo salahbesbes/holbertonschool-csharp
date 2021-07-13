212eeb08555cad53db1ba47ca1f6579b795e491d
salahbesbes <salah_besbes@ymail.com>
2021-07-13 11:25:18 +0100

    check last int

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int last_dig = number % 10;
+        if (last_dig > 5)
+        {
+            System.Console.WriteLine($"Last digit of {number} is {last_dig} and is greater than 5");
+        }
+        else if (last_dig < 6 && last_dig != 0)
+        {
+            System.Console.WriteLine($"Last digit of {number} is {last_dig} and is less than 6 and not 0");
+        }
+        else
+        {
+            System.Console.WriteLine($"Last digit of {number} is {last_dig} and is 0");
+        }
+    }
+}
