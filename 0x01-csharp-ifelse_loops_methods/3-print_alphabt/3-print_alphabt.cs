8a8c04f89c79fe98b8ef4b9a1f4d18907e48140e
salahbesbes <salah_besbes@ymail.com>
2021-07-13 11:48:21 +0100

    print alphabet

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int e = 'a'; e <= 'z'; e++)
+            {
+                if (e != 'q' && e != 'e')
+                {
+                    System.Console.Write((char)e);
+                }
+            }
+        }
+    }
+}
