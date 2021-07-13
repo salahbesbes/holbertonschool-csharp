755e63e6c661326e316e6d0a7aeee8a2782d79f8
salahbesbes <salah_besbes@ymail.com>
2021-07-13 13:01:26 +0100

    combo 1

--- 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -12,7 +12,7 @@ namespace _6_print_comb2
                 for (int nb = line + 1; nb < 10; nb++)
                 {
                     if (line * 10 + nb == 89) sep = "\n";
-                    System.Console.Write($"{line * 10 + nb}{sep}");
+                    System.Console.Write($"{line * 10 + nb:D2}{sep}");
                 }
             }
         }
