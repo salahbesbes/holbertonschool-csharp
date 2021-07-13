1020daeffdf4e8138134f4d9ab97f82b2c42913f
salahbesbes <salah_besbes@ymail.com>
2021-07-13 12:29:30 +0100

    check for lowercase

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        if ((int)c >= 'A' && (int)c <= 'Z') return false;
+        else return true;
+
+    }
+}
