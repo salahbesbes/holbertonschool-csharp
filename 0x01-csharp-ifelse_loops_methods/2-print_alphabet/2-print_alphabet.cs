c4b23a00f5bf3c8201b3bae987d6dc18598ca2e8
salahbesbes <salah_besbes@ymail.com>
2021-07-13 11:39:57 +0100

    print alphabet

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int e = 'a'; e <= 'z'; e++)
+            {
+                System.Console.Write((char)e);
+            }
+        }
+    }
+}
