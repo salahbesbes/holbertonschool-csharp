f932197e50b3c6e3685b9554fb3b0a17fd46ebca
salahbesbes <salah_besbes@ymail.com>
2021-07-14 23:29:01 +0100

    update matrix

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        for (int i = 0; i < myMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < myMatrix.GetLength(1); j++)
+            {
+                myMatrix[i, j] = (int)Math.Pow((int)myMatrix[i, j], 2);
+            }
+        }
+        return myMatrix;
+    }
+}
