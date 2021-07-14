cd8156e09c3f00b246c5a036e180df55c72f2c75
salahbesbes <salah_besbes@ymail.com>
2021-07-14 16:12:39 +0100

    check for even

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class List
+{
+    public static System.Collections.Generic.List<bool> DivisibleBy2(System.Collections.Generic.List<int> myList)
+    {
+        var boolenList = new System.Collections.Generic.List<bool>();
+        foreach (int i in myList)
+        {
+            boolenList.Add(i % 2 == 0);
+        }
+        return boolenList;
+    }
+}
