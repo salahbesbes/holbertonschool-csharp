4a2546dbab38d7890b03b3332392ee39757e469a
salahbesbes <salah_besbes@ymail.com>
2021-07-14 22:18:11 +0100

    get the biggest

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        var orderedValue = myList.OrderBy(item => item.Value);
+        return orderedValue.Last().Key;
+    }
+}
