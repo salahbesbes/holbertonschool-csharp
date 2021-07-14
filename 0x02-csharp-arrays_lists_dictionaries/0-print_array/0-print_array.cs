41451c69784812d8c4122fdd5b67cdf3835850e2
salahbesbes <salah_besbes@ymail.com>
2021-07-14 11:51:44 +0100

    declare array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return new int[0];
+        }
+        else if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else
+        {
+            int[] newArray = new int[size];
+            char sep = ' ';
+            for (int i = 0; i < size; i++)
+            {
+                if (i == size - 1) sep = '\n';
+                Console.Write($"{i}{sep}");
+                newArray[i] = i;
+            }
+
+            return newArray;
+
+        }
+    }
+}
