09a25c6cd4266156437a96d8a40ba513020367bf
salahbesbes <salah_besbes@ymail.com>
2021-07-14 19:16:52 +0100

    add key value

---
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Dictionary
+{
+    public static System.Collections.Generic.Dictionary<string, string> AddKeyValue(System.Collections.Generic.Dictionary<string, string> myDict, string key, string value)
+    {
+        myDict.Add(key, value);
+        return myDict;
+    }
+}
