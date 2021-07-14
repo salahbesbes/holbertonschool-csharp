671e496d8b86859d053221b6b525a32c287ddd63
salahbesbes <salah_besbes@ymail.com>
2021-07-14 12:08:48 +0100

    try catch

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        try
+        {
+            return array[index];
+        }
+        catch (System.Exception)
+        {
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
+    }
+}
