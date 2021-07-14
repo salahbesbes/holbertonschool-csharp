00fad27a542700bf1ea9e79a8a5b340acbbfa326
salahbesbes <salah_besbes@ymail.com>
2021-07-14 19:53:44 +0100

    remove from dict

---
+++ 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+class Dictionary
+{
+    public static System.Collections.Generic.Dictionary<string, string> DeleteKeyValue(System.Collections.Generic.Dictionary<string, string> myDict, string key)
+    {
+        myDict.Remove(key);
+        return myDict;
+    }
+}
