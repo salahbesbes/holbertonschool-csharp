8b408aaf1439633dea13485a412e0dba98bab74e
salahbesbes <salah_besbes@ymail.com>
2021-07-14 18:15:27 +0100

    list is mutable

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -4,7 +4,11 @@ class List
 {
     public static System.Collections.Generic.List<int> DeleteAt(System.Collections.Generic.List<int> myList, int index)
     {
-        myList.RemoveRange(index, 1);
+        if (index >= 0 && index < myList.Count)
+        {
+            myList.RemoveRange(index, 1);
+        }
+
         return myList;
     }
 }
