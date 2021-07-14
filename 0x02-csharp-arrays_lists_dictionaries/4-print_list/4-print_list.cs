28930de706dc1064341cee54be2bf1c119397ff5
salahbesbes <salah_besbes@ymail.com>
2021-07-14 14:54:29 +0100

    create new lst

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class List
+{
+    public static System.Collections.Generic.List<int> CreatePrint(int size)
+    {
+
+        var newList = new System.Collections.Generic.List<int>();
+        char sep = ' ';
+        for (int i = 0; i < size; i++)
+        {
+            if (i == size - 1) sep = '\n';
+            Console.Write($"{i}{sep}");
+            newList.Add(i);
+        }
+
+        return newList;
+    }
+}
