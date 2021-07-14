9faade785989597c64f0122117133d2c1a44ec62
salahbesbes <salah_besbes@ymail.com>
2021-07-14 13:14:30 +0100

    reverse array

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        char sep = ' ';
+        array.Reverse();
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i == array.Length - 1) sep = '\n';
+            Console.Write($"{array[i]}{sep}");
+        }
+    }
+}
