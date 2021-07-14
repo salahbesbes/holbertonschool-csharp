702da5c05ae8a599c18641702d1d9600bf45d98d
salahbesbes <salah_besbes@ymail.com>
2021-07-14 16:00:40 +0100

    get max value

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class List
+{
+    public static int MaxInteger(System.Collections.Generic.List<int> myList)
+    {
+
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+
+        int MinValue = -2147483648;
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (myList[i] > MinValue)
+            {
+                MinValue = myList[i];
+            }
+        }
+
+        return MinValue;
+    }
+}
