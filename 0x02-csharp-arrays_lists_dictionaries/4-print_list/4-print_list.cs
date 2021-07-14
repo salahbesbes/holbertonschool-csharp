4f7ea768104fa60cf762c9147817399638e617ba
salahbesbes <salah_besbes@ymail.com>
2021-07-14 23:43:42 +0100

    init list

--- 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -1,19 +1,25 @@
 ﻿using System;
-
+using System.Collections.Generic;
 class List
 {
-    public static System.Collections.Generic.List<int> CreatePrint(int size)
+    public static List<int> CreatePrint(int size)
     {
 
-        var newList = new System.Collections.Generic.List<int>();
+        var newList = new List<int>();
 
         if (size < 0)
         {
             Console.WriteLine("Size cannot be negative");
             return null;
         }
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return newList;
+        }
 
         char sep = ' ';
+
         for (int i = 0; i < size; i++)
         {
             if (i == size - 1) sep = '\n';
