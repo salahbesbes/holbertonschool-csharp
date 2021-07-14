f8d7b098b555eadf8850101b6ee0e359dff746e3
salahbesbes <salah_besbes@ymail.com>
2021-07-14 20:25:39 +0100

    update dict

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        var newDict = new Dictionary<string, int>();
+
+        foreach (KeyValuePair<string, int> item in myDict)
+        {
+            Console.WriteLine(item.Value);
+            newDict.Add(item.Key, item.Value * 2);
+        }
+
+        return newDict;
+    }
+}
