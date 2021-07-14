15cb6f052797851ffd10d5fd67ad43db6ecc7577
salahbesbes <salah_besbes@ymail.com>
2021-07-14 20:51:16 +0100

    sort a dict

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var sortedDic = myDict.OrderBy(i => i.Key);
+        foreach (var item in sortedDic)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+    }
+}
