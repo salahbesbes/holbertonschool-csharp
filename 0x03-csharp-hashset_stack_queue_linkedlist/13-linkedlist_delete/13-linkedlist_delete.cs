d6291c6f72e5d5097026fe50631187d92346b51e
salahbesbes <salah_besbes@ymail.com>
2021-07-15 16:46:57 +0100

    remove node after some node

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        try
+        {
+            int value = myLList.Select((item, inx) => new { item, inx })
+              .First(el => el.inx == index).item;
+
+            myLList.Remove(value);
+        }
+        catch (System.Exception)
+        { }
+
+
+    }
+}
