00f5cf3df9c2e8c8a634ad72295d3ef087200265
salahbesbes <salah_besbes@ymail.com>
2021-07-15 14:32:25 +0100

    add new node

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        var newNode = new LinkedListNode<int>(n);
+        myLList.AddFirst(newNode);
+        return newNode;
+    }
+}
