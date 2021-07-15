cd5e63c6351a5093b3720d477fef514a349d7fa4
salahbesbes <salah_besbes@ymail.com>
2021-07-15 16:40:06 +0100

    insert node after some node

--- 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -6,17 +6,30 @@ class LList
 {
     public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
     {
-        int value = myLList.Select((item, inx) => new { item, inx })
+
+        try
+        {
+            int value = myLList.Select((item, inx) => new { item, inx })
            .First(el => el.item > n).item;
 
+            LinkedListNode<int> reff = myLList.Find(value);
 
-        LinkedListNode<int> reff = myLList.Find(value);
+            var newNode = new LinkedListNode<int>(n);
 
-        var newNode = new LinkedListNode<int>(n);
+            myLList.AddBefore(reff, newNode);
 
-        myLList.AddBefore(reff, newNode);
+            return newNode;
+        }
+        catch (System.Exception)
+        {
+            var newNode = new LinkedListNode<int>(n);
 
-        return newNode;
+            myLList.AddLast(newNode);
+
+            return newNode;
+        }
+
+
 
     }
 }
