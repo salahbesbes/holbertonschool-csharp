d14285ff8ef9f1455d664a5774fcb587c08fd886
salahbesbes <salah_besbes@ymail.com>
2021-07-15 12:06:39 +0100

    sort a list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        var result = new List<int>() { };
+
+        foreach (int item1 in list1)
+        {
+            if (list2.Contains(item1)) result.Add(item1);
+        }
+        result.Sort();
+        return result;
+
+    }
+}
