39b67e28593ed702cf3abd418cc3485375f37c29
salahbesbes <salah_besbes@ymail.com>
2021-07-15 15:44:49 +0100

    get value of linkedlist by the index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (myLList.Count == 0) return 0;
+
+        try
+        {
+
+            int value = myLList.Select((item, inx) => new { item, inx })
+                       .First(el => el.inx == n).item;
+            return value;
+        }
+        catch (System.Exception)
+        {
+            return 0;
+        }
+    }
+}
