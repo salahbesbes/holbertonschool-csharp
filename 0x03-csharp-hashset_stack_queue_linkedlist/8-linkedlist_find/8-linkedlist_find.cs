a6966ce782627e63683ad75fe280ab5e07711887
salahbesbes <salah_besbes@ymail.com>
2021-07-15 15:06:08 +0100

    return index of a some node value

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        try
+        {
+
+            int index = myLList.Select((item, inx) => new { item, inx })
+                       .First(x => x.item == value).inx;
+
+            return index;
+        }
+        catch (System.Exception)
+        {
+            return -1;
+        }
+    }
+}
