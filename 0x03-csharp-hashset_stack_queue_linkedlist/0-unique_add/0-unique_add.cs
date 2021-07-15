c54ae448ce3191d9059b19ed443b27c8226fbedf
salahbesbes <salah_besbes@ymail.com>
2021-07-15 11:06:30 +0100

    cast to hashset get sum

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        var hashSet = new HashSet<int>(myList);
+        int sum = 0;
+
+
+        foreach (int item in hashSet)
+        {
+            sum += item;
+        }
+        return sum;
+    }
+}
