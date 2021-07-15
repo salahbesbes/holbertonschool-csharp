6a651e26f587a1a0619d335a21601dc3a635fde3
salahbesbes <salah_besbes@ymail.com>
2021-07-15 13:02:04 +0100

    manage stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aStack.Count}");
+        if (aStack.Count == 0) Console.WriteLine("Stack is empty");
+        else Console.WriteLine($"Top item: {aStack.Peek()}");
+
+        Console.WriteLine($"Stack contains {search}: {aStack.Contains(search)}");
+
+        if (aStack.Contains(search))
+        {
+            string popped;
+            for (int i = 0; i < aStack.Count; i++)
+            {
+                popped = aStack.Pop();
+                if (popped == search) break;
+            }
+        }
+
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}
