d177cad1baedcf5d7176c42e47b0854f1641b2b3
salahbesbes <salah_besbes@ymail.com>
2021-07-15 12:19:09 +0100

    sort a list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        var result = new List<int>(list1);
+
+        foreach (int item2 in list2)
+        {
+            if (result.Contains(item2) == false) result.Add(item2);
+            else result.Remove(item2);
+        }
+        result.Sort();
+        return result;
+
+    }
+}
