ff586b532f2b3ae2819788c8b59805b0547c68d8
salahbesbes <salah_besbes@ymail.com>
2021-07-15 14:19:05 +0100

    create linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        var myLinkedList = new LinkedList<int>();
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine(i);
+            myLinkedList.AddFirst(i);
+        }
+        return myLinkedList;
+    }
+}
