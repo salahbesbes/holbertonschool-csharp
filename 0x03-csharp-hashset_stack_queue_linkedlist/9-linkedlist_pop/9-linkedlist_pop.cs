946912fa4ca89bb993e4e2cca6628cb8673a03b6
salahbesbes <salah_besbes@ymail.com>
2021-07-15 15:32:46 +0100

    return the value the first node

--- 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -5,7 +5,7 @@ class LList
 {
     public static int Pop(LinkedList<int> myLList)
     {
-        if (myLList == null) return 0;
+        if (myLList.Count == null) return 0;
 
         var deletedNode = myLList.First;
         myLList.Remove(deletedNode);
