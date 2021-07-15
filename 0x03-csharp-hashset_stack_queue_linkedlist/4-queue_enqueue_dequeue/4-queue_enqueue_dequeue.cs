36e7d42f001a1abd9e74d8363bd355bbebefd6c6
salahbesbes <salah_besbes@ymail.com>
2021-07-15 13:23:05 +0100

    manage Queue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aQueue.Count}");
+
+        if (aQueue.Count == 0) Console.WriteLine("Queue is empty");
+        else Console.WriteLine($"First item: {aQueue.Peek()}");
+
+        aQueue.Enqueue(newItem);
+        if (aQueue.Contains(search)) Console.WriteLine($"Queue contains \"{search}\": {aQueue.Contains(search)}");
+
+
+        if (aQueue.Contains(search))
+        {
+            string removed;
+
+            for (int i = 0; i < aQueue.Count; i++)
+            {
+                removed = aQueue.Dequeue();
+                if (removed == search) break;
+
+            }
+        }
+        return aQueue;
+    }
+}
