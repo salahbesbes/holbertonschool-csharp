5bf12e7692d3f92b612f8deba5216001c93b4d12
salahbesbes <salah_besbes@ymail.com>
2021-07-26 18:08:42 +0100

    override toString

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+public enum Rating
+{ Good, Great, Excellent }
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+
+    public override string ToString()
+    {
+        return String.Join(Environment.NewLine,
+            $"Dog Name: {this.name}",
+            $"Age: {this.age}",
+            $"Owner: {this.owner}",
+            $"Rating: {this.rating}");
+    }
+}
