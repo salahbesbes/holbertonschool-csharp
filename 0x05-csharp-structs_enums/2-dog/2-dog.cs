23fcbc41427d7d4e502f74ae85815b4d5199b322
salahbesbes <salah_besbes@ymail.com>
2021-07-26 17:37:11 +0100

    craete a constractor

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,20 @@
+﻿
+public enum Rating
+{ Good, Great, Excellent }
+
+struct Dog
+{
+
+    public string name { get; }
+    public float age { get; }
+    public string owner { get; }
+    public Rating rating { get; }
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+
+}
