66b2e45c1d7094e8ed6133a9bb33b6c65448e46a
salahbesbes <salah_besbes@ymail.com>
2021-07-27 17:45:55 +0100

    ovveride tostring

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace Enemies
+{
+
+    class Zombie
+    {
+        private int health;
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public Zombie()
+        {
+            health = 0;
+            name = "(No name)";
+
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0) throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+            name = "(No name)";
+        }
+
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        public override string ToString()
+        {
+            return $"Zombie Name: {name} / Total Health: {health}";
+        }
+    }
+}
