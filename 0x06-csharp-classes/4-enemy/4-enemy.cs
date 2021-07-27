eda44f169fe81bb521f8b0f34b5768e47e299165
salahbesbes <salah_besbes@ymail.com>
2021-07-27 17:35:37 +0100

    create property to update private field

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Enemies
+{
+
+    class Zombie
+    {
+        private int health;
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public Zombie()
+        {
+            health = 0;
+            name = "(No name)";
+
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0) throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+            name = "(No name)";
+        }
+
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
