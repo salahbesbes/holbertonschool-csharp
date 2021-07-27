14715537224807fe759d3dc44a78afc3a2bb0688
salahbesbes <salah_besbes@ymail.com>
2021-07-27 17:13:16 +0100

    craete private field

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Enemies
+{
+
+    class Zombie
+    {
+        private int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0) throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
