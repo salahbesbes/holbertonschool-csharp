fcaa018c85d213278e76bcdcc7f0adc6df4b69d0
salahbesbes <salah_besbes@ymail.com>
2021-07-29 22:14:19 +0100

    update test

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -4,15 +4,31 @@ namespace MyMath.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Divide_Matrix_Not_empty()
         {
+            int[,] matrix = new int[,] { { 4, 2 }, { 2, 3 }, { 3, 0 } };
+            int num = 2;
+            int[,] expected = new int[,] { { 2, 1 }, { 1, 1 }, { 1, 0 } };
+            Assert.AreEqual(expected, Matrix.Divide(matrix, num));
         }
 
+
         [Test]
-        public void Test1()
+        public void Divide_NullMatrixBy_Number()
         {
-            Assert.Pass();
+            int[,] matrix = null;
+            int num = 2;
+            Assert.AreEqual(null, Matrix.Divide(matrix, num));
+        }
+
+
+        [Test]
+        public void Divide_NumberEqual_To_0()
+        {
+            int[,] matrix = new int[,] { { 4, 2 }, { 2, 3 }, { 3, 0 } };
+            int[,] expected = null;
+            Assert.AreEqual(expected, Matrix.Divide(matrix, 0));
         }
     }
 }
