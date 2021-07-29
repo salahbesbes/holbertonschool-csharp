084728f9bc6901286d2cc7d3850a31a9a03ef0c5
salahbesbes <salah_besbes@ymail.com>
2021-07-29 19:58:21 +0100

    update

--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -2,8 +2,13 @@
 
 namespace MyMath
 {
+    /// <summary> Mathemathics </summary>
     public class Operations
     {
+        /// <summary> add operation </summary>
+        /// <param name="a">First integer </param>
+        /// <param name="b">Second integer</param>
+        /// <returns>Addition: a + b.</returns>
         public static int Add(int a, int b)
         {
             return a + b;
