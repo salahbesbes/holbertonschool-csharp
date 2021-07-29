bfc33d2bf4690216e451afd15738fa25e9b445f3
salahbesbes <salah_besbes@ymail.com>
2021-07-29 21:51:21 +0100

    update matrix

--- 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -2,10 +2,26 @@
 
 namespace MyMath
 {
+
+    /// <summary>Divide matrix</summary
+
     public class Matrix
     {
+
+        /// <summary> divide matrix </summary>
+        /// <param name="matrix">mateix </param>
+        /// <param name="num">number we want to divide with </param>
+        /// <returns>modified matrix </returns>
         public static int[,] Divide(int[,] matrix, int num)
         {
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] /= num;
+                }
+            }
             return matrix;
         }
     }
