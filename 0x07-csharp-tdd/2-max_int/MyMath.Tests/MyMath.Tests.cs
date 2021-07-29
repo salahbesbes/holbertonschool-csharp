7071bcb0442e36a81b234ab39720234c0a52d0f3
salahbesbes <salah_besbes@ymail.com>
2021-07-29 19:34:31 +0100

    max int

---
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/2-max_int/MyMath/Operations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyMath
+{
+    public class Operations
+    {
+        public static int Max(List<int> nums)
+        {
+            if (nums == null || nums.Count == 0 )
+            {
+                return 0;
+            }
+
+            return nums.Max();
+        }
+    }
+}
