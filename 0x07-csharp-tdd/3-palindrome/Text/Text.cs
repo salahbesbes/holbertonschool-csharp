af5a07456416c0d2ea55af0bd657e15522e74e59
salahbesbes <salah_besbes@ymail.com>
2021-07-31 18:45:53 +0100

    check for equals

--- 0x07-csharp-tdd/3-palindrome/Text/Text.cs
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -37,6 +37,7 @@ namespace Text
             string stripped =  eliminatePunctuation(s);
             stripped = stripped.Replace(" ", "");
             int length = stripped.Length;
+            Console.WriteLine(stripped);
             string reverse  = "";
 
             for (int i = length - 1; i >= 0; i--)
