316bd5681ac1c20b7c82ed59dfe8e3402afe099d
salahbesbes <salah_besbes@ymail.com>
2021-07-31 18:36:10 +0100

    palindromic string

---
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase("earae")]
+        [TestCase("ACCCA")]
+        [TestCase("Bvccvb")]
+        [TestCase("A man, a plan, a canal: Panama.")]
+        [TestCase("")]
+        public void TestCasesExpectedTrue(string input)
+        {
+            bool expected = true;
+            bool res = Str.IsPalindrome(input);
+            Assert.AreEqual(expected, res);
+        }
+
+        [Test]
+        [TestCase("salah")]
+        [TestCase("Salah")]
+        [TestCase(null)]
+        public void TestCasesExpectedFalse(string input)
+        {
+            bool expected = false;
+            bool res = Str.IsPalindrome(input);
+            Assert.AreEqual(expected, res);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+namespace Text
+{
+    /// <summary>
+    /// class Str
+    /// </summary>
+    public class Str
+    {
+        /// <summary> create new string withoud penctuation  </summary>
+        /// <param name="input"> bute string </param>
+        /// <returns> new string with no punctuation </returns>
+        public static string eliminatePunctuation(string input)
+        {
+            var sb = new StringBuilder();
+            foreach (char c in input)
+            {
+                if (!char.IsPunctuation(c))
+                    sb.Append(c);
+            }
+            return sb.ToString();
+        }
+        
+        
+        /// <summary>  check if th string is palindromic  </summary>
+        /// <param name="s"> input </param>
+        /// <returns> boolen  </returns>
+        public static bool IsPalindrome(string s)
+        {
+            if (s == null) return false;
+
+            s = s.ToLower();
+
+            // eliminate penctuation
+            
+            string stripped =  eliminatePunctuation(s);
+            stripped = stripped.Replace(" ", "");
+            int length = stripped.Length;
+            Console.WriteLine(stripped);
+            string reverse  = "";
+
+            for (int i = length - 1; i >= 0; i--)
+            {
+                reverse += stripped[i];
+            }
+            return stripped == reverse;
+        }
+    }
+}
