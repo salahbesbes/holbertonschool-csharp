d52b664c1dd548423efd084b0a50b04888ed66e3
salahbesbes <salah_besbes@ymail.com>
2021-08-02 16:03:55 +0100

    print all attribute and methods of a Type

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+/// <summary> Class Obj </summary>
+internal class Obj
+{
+	/// <summary> prints the names of the available properties and methods of an object. </summary>
+	/// <param name="myObj"> any Type Obj </param>
+	public static void Print(object myObj)
+	{
+		MethodInfo[] listMethods = myObj.GetType().GetMethods();
+		PropertyInfo[] listproperties = myObj.GetType().GetProperties();
+
+		Console.WriteLine("Int32 Properties:");
+		foreach (var item in listproperties)
+		{
+			Console.WriteLine(item.Name);
+		}
+
+		Console.WriteLine("Int32 Methods:");
+		foreach (var item in listMethods)
+		{
+			Console.WriteLine(item.Name);
+		}
+	}
+}
