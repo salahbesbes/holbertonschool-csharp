0873bb48a064493cb85451f2e09b2d4757a7227c
salahbesbes <salah_besbes@ymail.com>
2021-08-02 20:51:34 +0100

    update Square classs

--- 0x08-csharp-inheritance/8-shape/8-shape.cs
+++ 0x08-csharp-inheritance/8-shape/8-shape.cs
@@ -11,7 +11,7 @@ class Square : Rectangle
     {
         get => size; set
         {
-            if (value < 0) throw new ArgumentException("ArgumentException ");
+            if (value < 0) throw new ArgumentException("Size must be greater than or equal to 0");
             else { Width = value; Height = value; size = value; }
             }
     }
