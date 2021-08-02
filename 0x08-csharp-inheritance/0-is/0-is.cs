16147271a81e9ecf2513bf9c5106aabf72254343
salahbesbes <salah_besbes@ymail.com>
2021-08-02 14:03:42 +0100

    check for type

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,11 @@
+﻿/// <summary> Obj class </summary>
+internal class Obj
+{
+	/// <summary> check if the element passed is int </summary>
+	/// <param name="obj"> any type obj </param>
+	/// <returns> true if its int else false </returns>
+	public static bool IsOfTypeInt(object obj)
+	{
+		return obj.GetType() == typeof(int);
+	}
+}
