bc3e32f67fd03a5f7abf468d606e52d5e12a0871
salahbesbes <salah_besbes@ymail.com>
2021-08-02 14:49:18 +0100

    c

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 
+/// <summary> class Obj </summary>
 internal class Obj
 {
+	/// <summary> check if the obj is an instance of Array </summary>
+	/// <param name="obj"> any type </param>
+	/// <returns> boolean </returns>
 	public static bool IsInstanceOfArray(object obj)
 	{
 		return obj is Array;
