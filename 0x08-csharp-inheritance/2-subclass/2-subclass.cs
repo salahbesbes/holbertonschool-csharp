cc9597cbad241582601d401fd26f7e8231423e5e
salahbesbes <salah_besbes@ymail.com>
2021-08-02 14:59:55 +0100

    check for derived type and base type

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary> Class Obj </summary>
+internal class Obj
+{
+	/// <summary> check for instance of a class that inherits from the specified class </summary>
+	/// <param name="derivedType"> obj instance </param>
+	/// <param name="baseType"> obj base </param>
+	/// <returns> booolean </returns>
+	public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+	{
+		return derivedType.IsSubclassOf(baseType);
+	}
+}
