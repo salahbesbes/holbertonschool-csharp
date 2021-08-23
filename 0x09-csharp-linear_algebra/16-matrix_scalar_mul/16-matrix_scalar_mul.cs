c67ef406106ce93522dd29ab98c41c51e0447d7f
salahbesbes <salah_besbes@ymail.com>
2021-08-23 18:44:24 +0100

    multiply scalar

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary> matrix math </summary>
+class MatrixMath
+{
+    /// <summary> multiply scalar  </summary>
+    /// <param name="matrix">matrix </param>
+    /// <param name="scalar"> scalar </param>
+    /// <returns> new matrix </returns>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        if ((matrix.GetLength(0) == 3 || matrix.GetLength(0) == 2)
+            && matrix.GetLength(0) == matrix.GetLength(1))
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] *= scalar;
+                }
+            }
+
+
+            return matrix;
+        }
+        return new double[,] { { -1 } };
+    }
+}
