8bb30239218a49e934b61027e18f17aef3205c47
salahbesbes <salah_besbes@ymail.com>
2021-08-23 14:22:16 +0100

    scalar * vector

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary> Vector Math Class  </summary>
+public class VectorMath
+{
+    /// <summary> calculate the multiplication of a vector </summary>
+    /// <param name="vector">vector</param>
+    /// <param name="scalar">multiple</param>
+    /// <returns> new vector </returns>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] error = new double[1] { -1 };
+        if (vector.Length != 2 && vector.Length != 3)
+            return error;
+
+
+        foreach (var point in vector)
+        {
+            Math.Round(point * scalar, 2);
+        }
+
+        return vector;
+    }
+}
