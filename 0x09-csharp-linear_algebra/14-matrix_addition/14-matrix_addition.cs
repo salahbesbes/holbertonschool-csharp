a6632b322f792c10fb58316f9d8e9e0e8d5a1049
salahbesbes <salah_besbes@ymail.com>
2021-08-23 18:07:32 +0100

    add 2 matrix

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 
-class MatrixMath
+/// <summary> math matrix  </summary>
+public class MatrixMath
 {
+    /// <summary> add 2 matrix </summary>
+    /// <param name="matrix1">matrix 1</param>
+    /// <param name="matrix2">matrix 2</param>
+    /// <returns> matrix </returns>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
 
-        double[,] error = new double[,] { { -1 } };
+        double[,] error = new double[1, 1] { { -1 } };
+
         if (matrix1.GetLength(0) != matrix2.GetLength(0))
             return error;
         if (matrix1.GetLength(1) != 2 && matrix1.GetLength(1) != 3)
@@ -13,7 +19,7 @@ class MatrixMath
         if (matrix2.GetLength(1) != 2 && matrix2.GetLength(1) != 3)
             return error;
 
-        double[,] res = new double[matrix1.GetLength(0), matrix1.GetLength(0)];
+        double[,] res = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
 
         for (int i = 0; i < matrix1.GetLength(0); i++)
         {
