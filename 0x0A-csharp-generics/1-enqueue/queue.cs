0b272a3043506299e612f453a0baa5a07bde42a3
salahbesbes <salah_besbes@ymail.com>
2021-10-25 13:53:20 +0100

    engue nodes

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,48 @@
+﻿/// <summary> Geric </summary>
+/// <typeparam name="T"> Any Type </typeparam>
+internal class Queue<T>
+{
+	public static int count = 0;
+	public Node head;
+	public Node tail;
+
+	/// <summary> return a type of </summary>
+	/// <returns> string </returns>
+	public string CheckType()
+	{
+		return $"{typeof(T)}";
+	}
+
+	/// <summary> return the number of nodes </summary>
+	/// <returns> count var </returns>
+	public int Count()
+	{
+		return count;
+	}
+
+	/// <summary> add new node to the tail of the queue </summary>
+	/// <param name="newNode"> </param>
+	public void Enqueue(T value)
+	{
+		Node newNode = new Node(value);
+		if (count == 0) head = newNode;
+		else if (count == 1) tail = newNode;
+		else
+		{
+			tail.next = newNode;
+		}
+		count++;
+	}
+
+	/// <summary> Node encapsulate the data we want to work on </summary>
+	public class Node
+	{
+		public Node next;
+		public T value = default(T);
+
+		public Node(T value)
+		{
+			this.value = value;
+		}
+	}
+}
