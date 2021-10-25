f42ec78157d556e4b6e350ada599e9b5216dc588
salahbesbes <salah_besbes@ymail.com>
2021-10-25 14:52:04 +0100

    peek + print method

---
+++ 0x0A-csharp-generics/4-print/queue.cs
@@ -0,0 +1,106 @@
+﻿/// <summary> Geric </summary>
+/// <typeparam name="T"> Any Type </typeparam>
+public class Queue<T>
+{
+	/// <summary> head queque </summary>
+	public Node head { get; set; }
+
+	/// <summary> tail queque </summary>
+	public Node tail { get; set; }
+
+	/// <summary> static var of the count of element </summary>
+	public static int count { get; set; } = 0;
+
+	/// <summary> return a type of </summary>
+	/// <returns> string </returns>
+	public string CheckType()
+	{
+		return $"{typeof(T)}";
+	}
+
+	/// <summary> return the number of nodes </summary>
+	/// <returns> count var </returns>
+	public int Count()
+	{
+		return count;
+	}
+
+	/// <summary> add new node to the tail of the queue </summary>
+	/// <param name="value"> any type </param>
+	public void Enqueue(T value)
+	{
+		Node newNode = new Node(value);
+		if (count == 0)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail.next = newNode;
+			tail = newNode;
+		}
+		count++;
+	}
+
+	/// <summary> remove the head node and return it </summary>
+	/// <returns> old heal node </returns>
+	public T Dequeue()
+	{
+		if (count == 0)
+		{
+			System.Console.WriteLine("Queue is empty");
+			return default(T);
+		}
+
+		Node tmp = head;
+		head = head.next;
+		count--;
+		return tmp.value;
+	}
+
+	/// <summary> take a look at the value of the first node </summary>
+	/// <returns> value of the the head </returns>
+	public T Peek()
+	{
+		if (count == 0)
+		{
+			System.Console.WriteLine("Queue is empty");
+			return default(T);
+		}
+		return head.value;
+	}
+
+	/// <summary> print the queue </summary>
+	public void Print()
+	{
+		if (count == 0)
+		{
+			System.Console.WriteLine("Queue is empty");
+			return;
+		}
+		Node current = head;
+		while (current != null)
+		{
+			System.Console.WriteLine($"{current.value}");
+			current = current.next;
+		}
+	}
+
+	/// <summary> Node encapsulate the data we want to work on </summary>
+	public class Node
+	{
+		/// <summary> next Node </summary>
+		public Node next { get; set; }
+
+		/// <summary> data to store </summary>
+		public T value { get; set; } = default(T);
+
+		/// <summary> Node Constructor </summary>
+		/// <param name="value"> any type of value </param>
+		public Node(T value)
+		{
+			this.value = value;
+		}
+	}
+}
