4ed23c14bd0b5330bd087ad32c92af46783ecb70
salahbesbes <salah_besbes@ymail.com>
2021-10-26 22:10:47 +0100

    interface

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,66 @@
+﻿/// <summary> interface </summary>
+public interface IBreakable
+{
+	/// <summary> durability </summary>
+	int durability { get; set; }
+
+	/// <summary> break method </summary>
+	void Break();
+}
+
+/// <summary> interface </summary>
+public interface ICollectable
+{
+	/// <summary> status </summary>
+	bool isCollected { get; set; }
+
+	/// <summary> methode </summary>
+	void Collect();
+}
+
+/// <summary> interface </summary>
+public interface IInteractive
+{
+	/// <summary> interactable </summary>
+	void Interact();
+}
+
+/// <summary> abstarct class </summary>
+public abstract class Base
+{
+	/// <summary> name string </summary>
+	public string name { get; set; }
+
+	/// <summary> override to string methode </summary>
+	/// <returns> </returns>
+	public override string ToString()
+	{
+		return $"{name} is a {GetType()}";
+	}
+}
+
+/// <summary> test Class inhirits 1 class an 3 inteface </summary>
+public class TestObject : Base, IBreakable, ICollectable, IInteractive
+{
+	/// <summary> durability </summary>
+	public int durability { get; set; }
+
+	/// <summary> collected or not </summary>
+
+	public bool isCollected { get; set; }
+
+	/// <summary> implement hte break methode </summary>
+	public void Break()
+	{
+	}
+
+	/// <summary> implement hte collect methode </summary>
+	public void Collect()
+	{
+	}
+
+	/// <summary> implement hte interact methode </summary>
+	public void Interact()
+	{
+	}
+}
