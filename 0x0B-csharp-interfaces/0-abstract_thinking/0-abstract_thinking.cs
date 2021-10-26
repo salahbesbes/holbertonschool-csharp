f9a943c0a4374572a7a2e6e4b85a2664fc052a7d
salahbesbes <salah_besbes@ymail.com>
2021-10-26 21:55:02 +0100

    task 0

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,13 @@
+﻿/// <summary> abstarct class </summary>
+internal abstract class Base
+{
+	/// <summary> name string </summary>
+	public string name { get; set; }
+
+	/// <summary> override to string methode </summary>
+	/// <returns> </returns>
+	public override string ToString()
+	{
+		return $"{name} is a {GetType()}";
+	}
+}
---
+++ 0x0B-csharp-interfaces/README.md
@@ -0,0 +1,1 @@
+# abstarct + interface Class
