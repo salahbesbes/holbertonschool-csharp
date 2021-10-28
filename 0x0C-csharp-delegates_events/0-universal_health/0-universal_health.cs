122a858755c1f5f688319f00ed4eaf4b012bc7cc
salahbesbes <salah_besbes@ymail.com>
2021-10-28 11:54:56 +0100

    PLAYER CLASS

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/// <summary> class player </summary>
+public class Player
+{
+        /// <summary> name props </summary>
+        private string name { get; set; } = "Player";
+
+        /// <summary> maxHP props </summary>
+        private float maxHp { get; set; } = 100f;
+
+        /// <summary> hp props </summary>
+        private float hp { get; set; }
+
+        /// <summary> constructor </summary>
+        /// <param name="name"> </param>
+        /// <param name="maxHp"> </param>
+        public Player(string name, float maxHp)
+        {
+                if (maxHp <= 0)
+                {
+                        maxHp = 100f;
+                        Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+                }
+                this.name = name;
+                this.maxHp = maxHp;
+                hp = maxHp;
+        }
+
+        /// <summary> print health </summary>
+        public void PrintHealth()
+        {
+                Console.WriteLine($"{name} has {hp} / {maxHp} health");
+        }
+}
